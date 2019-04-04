4b05c627bf8ac862ab5e9095e704e9079003b7e1
jordan birkeland <jbirkeland@mcad.edu>
2019-04-03 19:52:15 -0500

    added postcard array to new Vue

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
-var postcardArray = [
-    {
-        location: "san diego",
-        year: 2019,
-        landmark: "sunset cliffs",
-        description: "architecture and scenery from the cliffs",
-    },
-    {
-        location: "arizona",
-        year: 2002,
-        landmark: "grand canyon",
-        description: "best family vacation ever",
-    },
-    {
-        location: "banff",
-        year: 1995,
-        landmark: "chateau lake louise",
-        description: "on the shores of lake louise",
-    },
-    {
-        location: "bar harbor",
-        year: 2017,
-        landmark: "acadia national park",
-        description: "you have to try the lobster pizza",
-    },
-    {
-        location: "north shore",
-        year: 1999,
-        landmark: "split rock lighthouse",
-        description: "the lighthouse sits majestically on the cliff",
-    }
-];
-
 new Vue({
     el: "#app",
     data:{
-
+        postcardArray: [
+            {
+                location: "san diego",
+                year: 2019,
+                landmark: "sunset cliffs",
+                description: "architecture and scenery from the cliffs",
+            },
+            {
+                location: "arizona",
+                year: 2002,
+                landmark: "grand canyon",
+                description: "best family vacation ever",
+            },
+            {
+                location: "banff",
+                year: 1995,
+                landmark: "chateau lake louise",
+                description: "on the shores of lake louise",
+            },
+            {
+                location: "bar harbor",
+                year: 2017,
+                landmark: "acadia national park",
+                description: "you have to try the lobster pizza",
+            },
+            {
+                location: "north shore",
+                year: 1999,
+                landmark: "split rock lighthouse",
+                description: "the lighthouse sits majestically on the cliff",
+            }
+        ]
     }
 });
